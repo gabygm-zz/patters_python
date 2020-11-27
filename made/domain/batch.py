bedd095f11c81d6fb4578c3c7add28caccaa9929
Gaby G <paogaby0803@gmail.com>
2020-11-27 00:45:30 -0500

    add model batch and order_line and test for batch

---
+++ made/domain/batch.py
@@ -0,0 +1,16 @@
+from made.domain.order_line import OrderLine
+from typing import Optional
+from datetime import date
+
+
+class Batch:
+    def __init__(
+        self, ref: str, sku: str, qty: int, eta: Optional[date]
+    ):
+        self.reference = ref
+        self.sku = sku
+        self.eta = eta
+        self.available_quantity = qty
+
+    def allocate(self, line: OrderLine):
+        self.available_quantity -= line.qty 
