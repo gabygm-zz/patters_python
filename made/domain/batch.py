3e6e65bd2fd6c0ee64a1031cbbff3b4bbd6793f3
Gaby G <paogaby0803@gmail.com>
2021-01-06 00:47:00 -0500

    deallocate line and add test

--- made/domain/batch.py
+++ made/domain/batch.py
@@ -10,10 +10,24 @@ class Batch:
         self.reference = ref
         self.sku = sku
         self.eta = eta
-        self.available_quantity = qty
+        self._purchased_quantity = qty
+        self._allocations = set()
 
     def allocate(self, line: OrderLine):
-        self.available_quantity -= line.qty 
+        if self.can_allocate(line):
+            self._allocations.add(line)
+ 
+    @property
+    def allocated_quantity(self) -> int:
+        return sum(line.qty for line in self._allocations)
+
+    @property
+    def available_quantity(self) -> int:
+        return self._purchased_quantity - self.allocated_quantity
 
     def can_allocate(self, line: OrderLine) -> bool:
-        return self.sku == line.sku and self.available_quantity >= line.qty    
+        return self.sku == line.sku and self.available_quantity >= line.qty
+
+    def deallocate_line_order(self, line:  OrderLine):
+        if line is self._allocations:
+            self._allocations.remove(line)
